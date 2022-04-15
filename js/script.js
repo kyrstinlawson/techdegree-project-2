9c20a187e70b3aba71b995a9b0aec797e4d457ae
kyrstinlawson <kyrstin.lawson@gmail.com>
2022-04-15 14:23:56 -0700

    Finished showPage function
    showPage function works and shows the first 9 students

--- js/script.js
+++ js/script.js
@@ -17,19 +17,34 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const studentList = document.querySelector(".student-list");
+
 
 function showPage(list, page) {
    let startIndex = (page * 9) - 9;
    let endIndex = (page * 9);
-   let studentList = document.getElementsByClassName("student-list");
    studentList.innerHTML = "";
    for (let i=0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         
-      }
+         let studentItem = 
+            `<li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src=${list[i].picture.thumbnail} alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+            </li>
+            `;
+            studentList.insertAdjacentHTML("beforeend", studentItem);
+      } 
+      
    }
-}
+};
 
+showPage(data, 1);
 
 
 /*
